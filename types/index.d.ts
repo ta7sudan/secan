d286ff28e99b4f715b93f6f0760551034c5d4f0c
ta7sudan <ta7sudan@gmail.com>
2019-08-26 14:40:17 +0800

    update d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,11 +1,11 @@
 export default function secan(options: {
-	debug: boolean | string,
-	breakIframe: boolean,
-	debuggerLoop: boolean,
-	interval: number,
-	hookFn: boolean,
-	baitURL: string,
-	allowInlineScript: boolean,
-	pageDomain: string,
-	scriptDomain: string | string[]
+	debug?: boolean | string,
+	breakIframe?: boolean,
+	debuggerLoop?: boolean,
+	interval?: number,
+	hookFn?: boolean,
+	baitURL?: string,
+	allowInlineScript?: boolean,
+	pageDomain?: string,
+	scriptDomain?: string | string[]
 }): void;
