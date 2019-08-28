c953bc76e69aac9768de79c4b72ccf36b64b3645
ta7sudan <ta7sudan@gmail.com>
2019-08-28 10:26:59 +0800

    update d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -9,3 +9,52 @@ export default function secan(options: {
 	pageDomain?: string,
 	scriptDomain?: string | string[]
 }): void;
+
+declare global {
+	interface WindowEventMap {
+		eval: EvalEvent;
+		alert: AlertEvent;
+		console: ConsoleEvent;
+		invaliddomain: InvalidDomainEvent;
+		sslbreak: void;
+		sslstrip: void;
+		iniframe: void;
+		headlessbrowser: void;
+		invalidscript: InvalidScriptEvent;
+		devtoolsopen: void;
+	}
+}
+
+export interface SecanEvent {
+	detail: any;
+}
+
+export interface EvalEvent extends SecanEvent {
+	detail: {
+		args: string;
+	};
+}
+
+export interface AlertEvent extends SecanEvent {
+	detail: {
+		args: string;
+	};
+}
+
+export interface ConsoleEvent extends SecanEvent {
+	detail: {
+		args: string;
+	};
+}
+
+export interface InvalidDomainEvent extends SecanEvent {
+	detail: {
+		url: string;
+	};
+}
+
+export interface InvalidScriptEvent extends SecanEvent {
+	detail: {
+		invalidScript: Array<string>;
+	};
+}
